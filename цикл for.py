5f9266b184f6ae8704d71ca948270529f33a4129
RocknRof1 <168718063+RocknRof1@users.noreply.github.com>
2024-05-21 12:37:35 +0300

    Add files via upload

---
+++ цикл for.py
@@ -0,0 +1,9 @@
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in list_:
+    car_count = 0
+    print(f'Я езжу на автомобиле марки {i}')
+
+for i in list_:
+    print(f'Я езжу на автомабиле марки {i}')
+    car_count += 10
+    print(car_count)
